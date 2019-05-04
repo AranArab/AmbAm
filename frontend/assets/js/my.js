08ac0aa8f0d2d0a1efc82c2eab4875784b80c06f
Aran Arab <sarab1@vols.utk.edu>
2019-05-04 14:15:02 -0400

    one more time

--- frontend/assets/js/my.js
+++ frontend/assets/js/my.js
@@ -14,7 +14,7 @@ new Vue({
     },
     methods: {
         submitData () {
-            console.log("working here")
+            //console.log("working here")
             // axios
             //     .get('https://api.coindesk.com/v1/bpi/currentprice.json')
             //     .then(response => (this.message = response.data.chartName))
@@ -23,8 +23,8 @@ new Vue({
                 .post('http://localhost:3606/api/signup', {Person_fname: this.Person_fname, Person_lname: this.Person_lname, phone_number: this.phone_number, Email: this.Email, 
             Username: this.Username, P_word: this.P_word, Diet: this.Diet})
                 .then(response => (
-                    this.msg = response.data.status,
-                    (this.msg == 'success') ? this.status = 'Added! Thanks.' : 'Ooops!'                                     
+                    this.status = response.data.status,
+                    (this.status == 'success') ? window.location = "signup.html" : this.msg = 'Added! Thanks.' : 'Ooops!'                                     
                     )                    
                 )             
                 // .then(() => ((this.msg == 'success')?this.status = 'Submitted! Thanks.':''))
